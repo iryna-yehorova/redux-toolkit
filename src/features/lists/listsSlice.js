6cb386bf4bb46b02645cc8f8cc0c0b9214484512
Iryna Yehorova <iryna.yehorova@faceit.com.ua>
2022-05-18 11:05:36 +0300

    add redux state and slice

--- src/features/lists/listsSlice.js
+++ src/features/lists/listsSlice.js
@@ -33,10 +33,60 @@ export const listsSlice = createSlice({
     name: 'list',
     initialState,
     reducers: {
+        createPet(state, action) {
+            const { pet } = action.payload;
+
+            pet.label = pet.name
+            pet.id = pet.id ? pet.id : Date.now()
+
+            if (pet.owner && this.owners && state.owners.length > 0) {
+                let owner = this.owners.find(o => o.label === pet.owner)
+                owner.pet = pet.name 
+            }
+
+            state.pets.push(pet)
+        },
+        updatePet(state, action) {
+            const { petId, update } = action.payload
+            const petIndexAtId = state.pets.findIndex( pet => pet.id === petId)
+            if (petIndexAtId > -1 && update) {
+                state.pets[petIndexAtId] = update
+            }
+        },
+        deletePet(state, action) {
+            const { petId } = action.payload
+            const petIndexAtId = state.pets.findIndex( pet => pet.id === petId)
+            if (petIndexAtId > -1) {
+                state.pets.splice(petIndexAtId, 1)
+            }
+        },
+        createOwner(state, action) {
+            const { owner } = action.payload
+            const newItem = {
+                ...owner.petOwner,
+                id: owner.id ? owner.id : Date.now(),
+                label: `${owner.petOwner.firstName} ${owner.petOwner.lastName}`
+            }
+            state.owners.push(newItem)
+        },
+        updateOwner(state, action) {
+            const { ownerId, update } = action.payload
+            const ownerIndexAtId = state.owners.findIndex( pet => pet.id === ownerId)
+            if (ownerIndexAtId > -1 && update) {
+                state.owners[ownerIndexAtId] = update
+            }
+        },
+        deleteOwner(state, action) {
+            const { ownerId } = action.payload
+            const ownerIndexAtId = state.owners.findIndex( owner => owner.id === ownerId)
+            if (ownerIndexAtId > -1) {
+                state.owners.splice(ownerIndexAtId, 1)
+            }
+        }
 
     }
 })
 
-export const {} = listsSlice.actions
+export const { createPet, updatePet, deletePet, createOwner, updateOwner, deleteOwner } = listsSlice.actions
 
 export default listsSlice.reducer
